44e67208161266b0996ed027dcd23e4f7bd2ffd4
Chris Lamb <chris@chris-lamb.co.uk>
2008-04-18 05:14:21 +0100

    Add mailparser package file
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

---
+++ DebianChangesBot/mailparsers/__init__.py
@@ -0,0 +1,6 @@
+from bug_closed import BugClosed
+from bug_submitted import BugSubmitted
+from security_announce import SecurityAnnounce
+from upload_accepted import UploadAccepted
+
+order = (upload_accepted, bug_closed, bug_submitted, security_announce)
