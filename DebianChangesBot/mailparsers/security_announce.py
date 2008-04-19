bac732f13272f38b5e9d58105f985844bb4ca7e0
Chris Lamb <chris@chris-lamb.co.uk>
2008-04-19 19:56:29 +0100

    Tighten SecurityAnnounceParser
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- DebianChangesBot/mailparsers/security_announce.py
+++ DebianChangesBot/mailparsers/security_announce.py
@@ -5,29 +5,34 @@ from DebianChangesBot.messages import SecurityAnnounceMessage
 
 import re
 
-SUBJECT = re.compile(r'^\[SECURITY\] \[DSA ([-\d]+)\] New ([^ ]+) packages fix (.*)$')
+SUBJECT = re.compile(r'^\[SECURITY\] \[DSA ([\d]+)-([\d+])\] New ([^ ]+) packages fix (.*)$')
 DATE = re.compile(r'(20\d\d)')
 
 class SecurityAnnounceParser(MailParser):
 
-    def parse(self, headers, body):
-        if headers['List-Id'] != '<debian-security-announce.lists.debian.org>':
+    @staticmethod
+    def parse(headers, body):
+        if headers.get('List-Id', '') != '<debian-security-announce.lists.debian.org>':
             return
 
         msg = SecurityAnnounceMessage()
 
-        m = SUBJECT.match(headers['Subject'])
+        m = SUBJECT.match(headers.get('Subject', ''))
         if m:
-            msg.dsa_number = m.group(1)
-            msg.package = m.group(2)
-            msg.problem = m.group(3)
-        else:
-            return
+            try:
+                msg.dsa_number = int(m.group(1))
+                msg.dsa_revision = int(m.group(2))
+            except ValueError:
+                return
 
-        m = DATE.search(headers['Date'])
+            msg.package = m.group(3)
+            msg.problem = m.group(4)
+
+        m = DATE.search(headers.get('Date', ''))
         if m:
-            msg.year = m.group(1)
-        else:
-            return
+            try:
+                msg.year = int(m.group(1))
+            except ValueError:
+                return
 
         return msg
