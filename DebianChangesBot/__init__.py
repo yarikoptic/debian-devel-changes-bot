903e3551d33248afd4dc9c3a7607b4fc14ad137a
Chris Lamb <chris@chris-lamb.co.uk>
2008-04-19 19:55:33 +0100

    Message objects should eval false in bool context if fields are missing
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- DebianChangesBot/__init__.py
+++ DebianChangesBot/__init__.py
@@ -12,8 +12,22 @@ class Datasource(object):
 class MailParser(object):
     pass
 
-
 class Message(object):
+    def __init__(self):
+        if hasattr(self, 'FIELDS'):
+            for field in self.FIELDS:
+                setattr(self, field, None)
+        if hasattr(self, 'OPTIONAL'):
+            for field in self.OPTIONAL:
+                setattr(self, field, None)
+
+    def __nonzero__(self):
+        if hasattr(self, 'FIELDS'):
+            for field in self.FIELDS:
+                if getattr(self, field) is None:
+                    return False
+        return True
+
     def format_email_address(self, email):
         email = re.sub(r'(?:<([-A-Za-z0-9]+)@debian.org>)', r'(\1)', email)
         email = re.sub(r'@(.{6}).*>', r'@\1..>', email)
