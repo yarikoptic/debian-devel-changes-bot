d4db7b14f6719058f799d86ae0e4a8875b09991b
Chris Lamb <chris@chris-lamb.co.uk>
2008-04-19 19:04:09 +0100

    messages/security_announce.py: Fix some syntax errors
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- DebianChangesBot/messages/security_announce.py
+++ DebianChangesBot/messages/security_announce.py
@@ -10,9 +10,9 @@ class SecurityAnnounceMessage(Message):
     FIELDS = ('dsa_number', 'package', 'problem', 'year')
 
     def format(self):
-        url = 'http://www.debian.org/security/%s/dsa-%s' %
+        url = 'http://www.debian.org/security/%s/dsa-%s' % \
             (self.year, PATTERN_DSA.sub('', self.dsa_number))
 
-        return "[red][Security][reset] ([yellow]DSA-%d[reset]) - "
+        return "[red][Security][reset] ([yellow]DSA-%d[reset]) - " \
             "New [green]%s[reset] packages fix %s. %(url)s" % \
             (self.dsa_number, self.package, self.problem, url)
