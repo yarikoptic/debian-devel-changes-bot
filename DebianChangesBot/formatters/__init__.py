a7c8f158c25b1f9f6158bc4d4740596b6fddf180
Chris Lamb <chris@chris-lamb.co.uk>
2008-04-19 05:28:41 +0100

    Add Formatter object (misnamed)
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- DebianChangesBot/formatters/__init__.py
+++ DebianChangesBot/formatters/__init__.py
@@ -1,5 +1,7 @@
 
-def format_email_address(self, email):
-    email = re.sub(r'(?:<([-A-Za-z0-9]+)@debian.org>)', r'(\1)', email)
-    email = re.sub(r'@(.{6}).*>', r'@\1..>', email)
-    return email.replace(r'"', '')
+class Formatter(object):
+
+    def format_email_address(self, email):
+        email = re.sub(r'(?:<([-A-Za-z0-9]+)@debian.org>)', r'(\1)', email)
+        email = re.sub(r'@(.{6}).*>', r'@\1..>', email)
+        return email.replace(r'"', '')
