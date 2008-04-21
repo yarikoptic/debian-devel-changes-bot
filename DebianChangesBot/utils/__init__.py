12395f86d2f43c93e75bdf34ca3cc489e019b34a
Chris Lamb <chris@chris-lamb.co.uk>
2008-04-21 05:31:07 +0100

    Add quoted_printable utility
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- DebianChangesBot/utils/__init__.py
+++ DebianChangesBot/utils/__init__.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import email.quoprimime
+
+def quoted_printable(val):
+    try:
+        if type(val) is str:
+            return email.quoprimime.header_decode(val)
+        else:
+            return unicode(email.quoprimime.header_decode(str(val)), 'utf-8')
+
+    except Exception, e:
+        # We ignore errors here. Most of these originate from a spam
+        # report adding a synopsis of a message with broken encodings.
+        pass
+
+    return val
+
 from parse_mail import parse_mail
