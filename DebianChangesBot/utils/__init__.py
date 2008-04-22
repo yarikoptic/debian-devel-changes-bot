6f30aed2b5f157bb22c8761a92464302ec5d8911
Chris Lamb <chris@chris-lamb.co.uk>
2008-04-22 02:05:54 +0100

    Update header_decode to handle bare and non-bare quoted-printable chars
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- DebianChangesBot/utils/__init__.py
+++ DebianChangesBot/utils/__init__.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
 
-import email.quoprimime
+import email
+import re
+
+def header_decode(s):
+    def unquote_match(match):
+        s = match.group(0)
+        return chr(int(s[1:3], 16))
+
+    s = s.replace('_', ' ')
+    return re.sub(r'=\w{2}', unquote_match, s)
 
 def quoted_printable(val):
     try:
         if type(val) is str:
-            return email.quoprimime.header_decode(val)
+            save = header_decode(val)
+            val = ' '.join([chunk.decode(encoding or 'ascii', 'replace') for chunk, encoding in
+                email.Header.decode_header(val)])
+
+            if len(val) > len(save):
+                val = unicode(save, 'utf-8', 'replace')
+
         else:
-            return unicode(email.quoprimime.header_decode(str(val)), 'utf-8')
+            return unicode(email.quoprimime.header_decode(str(val)), 'utf-8', 'replace')
 
     except Exception, e:
         # We ignore errors here. Most of these originate from a spam
