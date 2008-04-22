69f46b1ef0ece1c2979a9c7d0d54c4d8551298d6
Chris Lamb <chris@chris-lamb.co.uk>
2008-04-22 02:01:36 +0100

    Move datasources to Borg pattern, make thread safe, use accessor methods
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- DebianChangesBot/datasources/new_queue.py
+++ DebianChangesBot/datasources/new_queue.py
@@ -1,16 +1,43 @@
 # -*- coding: utf-8 -*-
 
+import thread
+import urllib2
 from BeautifulSoup import BeautifulSoup
 
 from DebianChangesBot import Datasource
 
 class NewQueue(Datasource):
+    __shared_state = {}
+
     URL = 'http://ftp-master.debian.org/new.html'
     INTERVAL = 60 * 30
 
-    def parse(self, fileobj):
+    packages = []
+    lock = thread.allocate_lock()
+
+    def __init__(self):
+        self.__dict__ = self.__shared_state
+
+    def update(self, fileobj=None):
+        if fileobj is None:
+            fileobj = urllib2.urlopen(self.URL)
+
         soup = BeautifulSoup(fileobj)
         cells = [row.find('td') for row in soup('tr', {'class': ('odd', 'even')})]
         packages = [str(c.string) for c in cells]
 
-        return packages
+        if len(packages) == 0:
+            raise Datasource.DataError()
+
+        self.lock.acquire()
+        try:
+            self.packages = packages
+        finally:
+            self.lock.release()
+
+    def is_new(self, package):
+        self.lock.acquire()
+        try:
+            return package in self.packages
+        finally:
+            self.lock.release()
--- DebianChangesBot/datasources/testing_rc_bugs.py
+++ DebianChangesBot/datasources/testing_rc_bugs.py
@@ -1,20 +1,43 @@
 # -*- coding: utf-8 -*-
 
 import re
+import thread
 
 from DebianChangesBot import Datasource
 
 class TestingRCBugs(Datasource):
+    __shared_state = {}
+
     URL = 'http://bts.turmzimmer.net/details.php?bydist=lenny'
+    BUG_COUNT = re.compile(r'.*Total shown: (?P<bug_count>\d+) bug.*')
     INTERVAL = 60 * 30
 
-    pattern = re.compile(r'.*Total shown: (?P<bug_count>\d+) bug.*')
+    lock = thread.allocate_lock()
+    num_bugs = None
 
-    def parse(self, fileobj):
+    def __init__(self):
+        self.__dict__ = self.__shared_state
+
+    def update(self, fileobj):
+        if fileobj is None:
+            fileobj = urllib2.urlopen(self.URL)
+
         for line in fileobj:
-            ret = self.pattern.match(line)
+            ret = self.BUG_COUNT.match(line)
             if ret:
-                val = ret.group('bug_count')
-                return int(val)
+                self.lock.acquire()
+                try:
+                    self.num_bugs = int(ret.group('bug_count'))
+                    return
+                finally:
+                    self.lock.release()
 
+        # No bug count line found
         raise Datasource.DataError()
+
+    def get_num_bugs(self):
+        self.lock.acquire()
+        try:
+            return self.num_bugs
+        finally:
+            self.lock.release()
