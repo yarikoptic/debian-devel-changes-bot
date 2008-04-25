4ae41ffe5d90e5812615e5cc53ac82513443daad
Jonny Lamb <jonnylamb@jonnylamb.com>
2008-04-25 17:07:52 +0100

    Wired up the format email unit tests
    Signed-off-by: Jonny Lamb <jonnylamb@jonnylamb.com>

--- DebianChangesBot/__init__.py
+++ DebianChangesBot/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import urllib2
+import re
 
 class Datasource(object):
     class DataError(Exception): pass
--- tests/test_utils_format_email.py
+++ tests/test_utils_format_email.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import unittest
 
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from DebianChangesBot import Message
+
 class TestFormatEmail(unittest.TestCase):
 
+    def setUp(self):
+        self.message = Message()
 
     def testSimple(self):
         pass
@@ -10,39 +19,53 @@ class TestFormatEmail(unittest.TestCase):
     def testDebian(self):
         val = "John Smith <jsmith@debian.org>"
         ret = "John Smith (jsmith)"
+        self.assertEqual(self.message.format_email_address(val), ret)
 
     def testDebianQuotes(self):
         val = '"John Smith" <jsmith@debian.org>'
         ret = "John Smith (jsmith)"
+        self.assertEqual(self.message.format_email_address(val), ret)
 
         val = "'John Smith' <jsmith@debian.org>"
         ret = "John Smith (jsmith)"
+        self.assertEqual(self.message.format_email_address(val), ret)
 
     def testUppercaseDebianEmail(self):
         val = 'John Smith <JSMITH@DEBIAN.ORG>'
         ret = "John Smith (jsmith)"
+        self.assertEqual(self.message.format_email_address(val), ret)
 
     def testUppercaseEmail(self):
         val = 'John Smith <JSMITH@HOST.TLD>'
         ret = "John Smith <jsmith@host.tld>"
+        self.assertEqual(self.message.format_email_address(val), ret)
 
     def testExtraSpacesOne(self):
         val = "John  Smith <jsmith@host.tld>"
         ret = "John Smith <jsmith@host.tld>"
+        self.assertEqual(self.message.format_email_address(val), ret)
 
         val = "John Smith  <jsmith@host.tld>"
         ret = "John Smith <jsmith@host.tld>"
+        self.assertEqual(self.message.format_email_address(val), ret)
 
     def testExtraSpacesTwo(self):
         val = "John Smith < jsmith@host.tld>"
         ret = "John Smith <jsmith@host.tld>"
+        self.assertEqual(self.message.format_email_address(val), ret)
 
         val = "John Smith < jsmith@host.tld>"
         ret = "John Smith <jsmith@host.tld >"
+        self.assertEqual(self.message.format_email_address(val), ret)
 
     def testDontRepeatName(self):
         val = "John Smith (jsmith) <jsmith@debian.org>"
         ret = "John Smith (jsmith)"
+        self.assertEqual(self.message.format_email_address(val), ret)
 
         val = "John Smith (JSMITH) <jsmith@debian.org>"
         ret = "John Smith (jsmith)"
+        self.assertEqual(self.message.format_email_address(val), ret)
+
+if __name__ == "__main__":
+    unittest.main()
